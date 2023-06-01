59c8888b8e6d3103319652a9685999abe0169654
Jiří Tumpach <tumpji@gmail.com>
2023-06-01 16:30:51 +0200

    SSH + SCP are running

--- metaconnect/connect.py
+++ metaconnect/connect.py
@@ -1,28 +1,71 @@
 #!/usr/bin/python3.11
-from collections import defaultdict, OrderedDict
-from io import StringIO
-import csv
-import operator
+import os
 import re
+import operator
+from collections import defaultdict, OrderedDict
+
+import csv
+import config_path
+import configparser
+
 import argparse
+import subprocess
 from fuzzywuzzy import process
 
-server_csv = '''
-skirit.ics.muni.cz 	Debian 11 	/storage/brno2/home/ 	meta-pbs.metacentrum.cz 	
-alfrid.meta.zcu.cz 	Debian 11 	/storage/plzen1/home/ 	meta-pbs.metacentrum.cz 	
-tarkil.grid.cesnet.cz 	Debian 11 	/storage/praha1/home/ 	meta-pbs.metacentrum.cz 	
-nympha.zcu.cz 	Debian 11 	/storage/plzen1/home/ 	meta-pbs.metacentrum.cz 	
-charon.nti.tul.cz 	Debian 11 	/storage/liberec3-tul/home/ 	meta-pbs.metacentrum.cz 	
-minos.zcu.cz 	Debian 11 	/storage/plzen1/home/ 	meta-pbs.metacentrum.cz 	alias for nympha.zcu.cz 	
-perian.grid.cesnet.cz 	Debian 10 	/storage/brno2/home/ 	meta-pbs.metacentrum.cz 	
-onyx.metacentrum.cz 	Debian 10 	/storage/brno2/home/ 	meta-pbs.metacentrum.cz 	alias for perian.grid.cesnet.cz
-tilia.ibot.cas.cz 	Debian 11 	/storage/pruhonice1-ibot/home/ 	meta-pbs.metacentrum.cz 	Cluster of the Institute of Botany, CAS
-zuphux.cerit-sc.cz 	CentOS 7.9 	/storage/brno3-cerit/home/ 	cerit-pbs.cerit-sc.cz 	
-elmo.elixir-czech.cz 	Debian 11 	/storage/praha5-elixir/home/ 	elixir-pbs.elixir-czech.cz 	
-''' + \
+
+def yes_or_no(question):
+    while True:
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        if reply[0] == 'y':
+            return True
+        if reply[0] == 'n':
+            return False
+
+
+class ConfigurationManager:
+    def __init__(self):
+        self.path_obj = config_path.ConfigPath('metaconnect', 'psb-pro-toolkit', '.ini')
+        self.directory_path = self.path_obj.saveFolderPath(mkdir=True)
+
+        self.config_path = os.path.join(self.directory_path, 'ssh.ini')
+        self.server_list_path = os.path.join(self.directory_path, 'servers.csv')
+
+        self.config = configparser.ConfigParser()
+
+        if os.path.exists(self.config_path):
+            self.config.read(self.config_path)
+        else:
+            self.config.read_string(self._default_config())
+            with open(self.config_path, 'w') as file:
+                self.config.write(file)
+
+        # server list
+        if os.path.exists(self.server_list_path):
+            path = self.server_list_path
+        else:
+            path = self._default_server_list_path()
+
+        self.server_list = []
+        with open(path, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter='\t')
+            for line in reader:
+                if len(line):
+                    self.server_list.append(line)
+
+    def _default_config(self):
+        return f'''[AUTHENTICATION]
+USER={os.getlogin()}
+KEYFILE=
 '''
-builder.metacentrum.cz	Debian 11	/storage/praha1/home/	meta-pbs.metacentrum.cz	
-'''
+
+    def _default_server_list_path(self):
+        return os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            'DEFAULT_SERVER_LIST.csv'
+        )
+
+    def __getitem__(self, item):
+        return self.config[item]
 
 
 def normalize_string(s):
@@ -88,11 +131,9 @@ class Node:
             setattr(cls, attrb, obj)
 
     @classmethod
-    def initialize(cls):
-        reader = csv.reader(StringIO(server_csv), delimiter='\t')
-        for x in reader:
-            if len(x):
-                Node(x)
+    def initialize(cls, configuration):
+        for x in configuration.server_list:
+            Node(x)
 
     @classmethod
     def print_options(cls):
@@ -105,7 +146,8 @@ class Node:
                 print(f'\t{i:<3} {x}')
 
 
-Node.initialize()
+configuration = ConfigurationManager()
+Node.initialize(configuration)
 Node.finalize()
 
 maybe_str_or_int_server = generate_str_or_int('server', Node.DICT_SERVER)
@@ -134,6 +176,7 @@ if __name__ == '__main__':
                         help='Name or index of an organization:\n' + '\n'.join(
                             f'\t{i}) {n}' for i,n in enumerate(Node.DICT_ORGANIZATION.keys())),
                         )
+    parser.add_argument('--scp', nargs=2, type=str)
 
     args = parser.parse_args()
 
@@ -147,8 +190,23 @@ if __name__ == '__main__':
         server = Node.ALL_NODES[0]
 
 
-    print('Processing:')
-    print(server)
+    print(f'Connecting to the {server}:')
+    print(f'\t{server.STORAGE}\t{server.ORGANIZATION}')
+    print('-'*60)
 
+    user = configuration['AUTHENTICATION']['USER']
+
+    if args.scp is not None:
+        subprocess.call([
+            'scp',
+            f'{user}@{server.SERVER_URL}:{args.scp[0]}',
+            args.scp[1]
+        ])
+    else:
+        subprocess.call([
+                'ssh',
+                f'{user}@{server.SERVER_URL}'
+            ]
+        )
 
 
